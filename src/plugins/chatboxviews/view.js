6d8991078aaae723632c4df572a04e4688a569f8
JC Brand <jc@opkode.com>
2020-12-08 12:54:14 +0100

    WIP: Turn chatbox views into custom elements
    The goal is to avoid UI-related stanza processing if the relevant chats
    aren't in the DOM.
    
    With the current architecture, chatboxes are created (and the stanzas
    related to them processed) even if `#conversejs` isn't in the DOM.

--- src/plugins/chatboxviews/view.js
+++ src/plugins/chatboxviews/view.js
@@ -1,75 +1,47 @@
 import tpl_background_logo from '../../templates/background_logo.js';
-import tpl_converse from '../../templates/converse.js';
-import { Overview } from '@converse/skeletor/src/overview';
+import tpl_chat from 'plugins/chatview/templates/chat.js';
+import tpl_controlbox from 'plugins/controlbox/templates/controlbox.js';
+import tpl_muc from 'plugins/muc-views/templates/muc.js';
 import { render } from 'lit-html';
-import { result } from 'lodash-es';
-import { _converse, api, converse } from '@converse/headless/core';
-
-const u = converse.env.utils;
+import { api, _converse } from '@converse/headless/core';
+import ElementView from '@converse/skeletor/src/element';
 
 
-const ChatBoxViews = Overview.extend({
-    _ensureElement () {
-        /* Override method from backbone.js
-         * If the #conversejs element doesn't exist, create it.
-         */
-        if (this.el) {
-            this.setElement(result(this, 'el'), false);
-        } else {
-            let el = _converse.root.querySelector('#conversejs');
-            if (el === null) {
-                el = document.createElement('div');
-                el.setAttribute('id', 'conversejs');
-                u.addClass(`theme-${api.settings.get('theme')}`, el);
-                const body = _converse.root.querySelector('body');
-                if (body) {
-                    body.appendChild(el);
-                } else {
-                    // Perhaps inside a web component?
-                    _converse.root.appendChild(el);
-                }
-            }
-            this.setElement(el, false);
-        }
-    },
+class ConverseChats extends ElementView {
 
     initialize () {
-        this.listenTo(this.model, 'destroy', this.removeChat);
+        this.listenTo(this.model, 'destroy', this.render);
+        this.listenTo(this.model, 'add', this.render);
+
         const bg = document.getElementById('conversejs-bg');
         if (bg && !bg.innerHTML.trim()) {
             render(tpl_background_logo(), bg);
         }
         const body = document.querySelector('body');
         body.classList.add(`converse-${api.settings.get('view_mode')}`);
-        this.el.classList.add(`converse-${api.settings.get('view_mode')}`);
-        if (api.settings.get('singleton')) {
-            this.el.classList.add(`converse-singleton`);
-        }
         this.render();
-    },
+    }
 
     render () {
-        this._ensureElement();
-        render(tpl_converse(), this.el);
-        this.row_el = this.el.querySelector('.row');
-    },
-
-    /**
-     * Add a new DOM element (likely a chat box) into the
-     * the row managed by this overview.
-     * @param { HTMLElement } el
-     */
-    insertRowColumn (el) {
-        this.row_el.insertAdjacentElement('afterBegin', el);
-    },
-
-    removeChat (item) {
-        this.remove(item.get('id'));
-    },
+        const { chatboxes, CONTROLBOX_TYPE, CHATROOMS_TYPE } = _converse;
+        if (chatboxes.length > 0) {
+            debugger;
+        }
+        const templates = chatboxes.map(m => {
+            if (m.get('type') === CONTROLBOX_TYPE) {
+                return tpl_controlbox(m);
+            } else if (m.get('type') === CHATROOMS_TYPE) {
+                return tpl_muc(m);
+            } else {
+                return tpl_chat(m);
+            }
+        });
+        render(templates, this);
+    }
 
     closeAllChatBoxes () {
-        return Promise.all(this.map(v => v.close({ 'name': 'closeAllChatBoxes' })));
+        return Promise.all(Array.from(this.children).map(v => v.close({ 'name': 'closeAllChatBoxes' })));
     }
-});
+}
 
-export default ChatBoxViews;
+api.elements.define('converse-chats', ConverseChats);
--- src/templates/converse.js
+++ src/templates/converse.js
@@ -1,9 +1,15 @@
-import { html } from 'lit-html';
 import '../components/font-awesome.js';
+import { api } from '@converse/headless/core';
+import { html } from 'lit-html';
 
-
-export default () => html`
-    <div class="converse-chatboxes row no-gutters"></div>
-    <div id="converse-modals" class="modals"></div>
-    <converse-fontawesome></converse-fontawesome>
-`;
+export default () => {
+    let extra_classes = api.settings.get('singleton') ? 'converse-singleton' : '';
+    extra_classes += `converse-${api.settings.get('view_mode')}`;
+    return html`
+        <converse-chats class="converse-chatboxes row no-gutters ${extra_classes}"></converse-chats>
+        <converse-login></converse-login>
+        <converse-register></converse-register>
+        <div id="converse-modals" class="modals"></div>
+        <converse-fontawesome></converse-fontawesome>
+    `;
+};
